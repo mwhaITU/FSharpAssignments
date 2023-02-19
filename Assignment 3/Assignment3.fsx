5aa4db9930513510ab51c9cf0d79014ea01eb950
Martin <mwha@itu.dk>
2023-02-19 14:14:12 +0100

    Finished some exercises from Assignment 3

--- Assignment 3/Assignment3.fsx
+++ Assignment 3/Assignment3.fsx
@@ -117,6 +117,7 @@ let rec charEval (c : cExp) (w : word) (s : state) =
 charEval (C 'H') [] Map.empty
 charEval (CV (V "x" .-. N 1)) hello (Map.ofList [("x", 5)])
 
+
 // 3.5
 type bExp = 
 | TT (* true *)
@@ -141,7 +142,7 @@ let (.>=.) a b = ~~(a .<. b) (* numeric greater than or equal to *)
 let (.>.) a b = ~~(a .=. b) .&&. (a .>=. b) (* numeric greater than *)
 
 let isVowel (c : char) =
-    if "aeiouy".Contains(System.Char.ToLower c) then true
+    if "aeiouyæøå".Contains(System.Char.ToLower c) then true
     else false
 
 let rec boolEval (b : bExp) (w : word) (s : state) =
@@ -165,3 +166,84 @@ boolEval (IsDigit (CV (V "x"))) hello (Map.ofList [("x", 4)])
 // Yellow Exercises
 
 // 3.6
+let isConsonant (c : cExp) = 
+    if "bcdfgjklmnpqstvxzhrwy".Contains(System.Char.ToLower(charEval c [] Map.empty)) then TT
+    else FF
+
+boolEval (isConsonant (C 'H')) [] Map.empty
+//boolEval (isConsonant (CV (V "x"))) hello (Map.ofList [("x", 0)])
+// boolEval (isConsonant (CV (V "x"))) hello (Map.ofList [("x", 1)])
+
+// 3.7 
+type stmnt =
+| Skip (* does nothing *)
+| Ass of string * aExp (* variable assignment *)
+| Seq of stmnt * stmnt (* sequential composition *)
+| ITE of bExp * stmnt * stmnt (* if-then-else statement *) 
+| While of bExp * stmnt (* while statement *)
+
+let rec evalStmnt (stm : stmnt) (w : word) (s : state) =
+    match stm with
+    | Skip -> s
+    | Ass (x, a) -> 
+        let v = arithEval a w s
+        let s' = s.Add (x, v)
+        s'
+    | Seq (stm1, stm2) -> 
+        let s' = evalStmnt stm1 w s
+        evalStmnt stm2 w s'
+    | ITE (guard, stm1, stm2) ->
+        let b = boolEval guard w s
+        if b = true then evalStmnt stm1 w s
+        else evalStmnt stm2 w s
+    | While (guard, stm) -> 
+        let b = boolEval guard w s
+        if b = true then
+            let s' = evalStmnt stm w s
+            evalStmnt (While(guard, stm)) w s'
+        else
+            s
+
+evalStmnt Skip [] Map.empty
+evalStmnt (Seq (Ass ("x", WL), Ass ("y", N 7))) hello Map.empty
+evalStmnt (ITE (WL .>=. N 5, Ass ("x", N 1), Ass ("x", N 2))) hello Map.empty
+evalStmnt (While (V "x" .<=. WL,
+ Seq (Ass ("y", V "y" .+. V "x"),
+ Ass ("x", V "x" .+. N 1))))
+ hello Map.empty;;
+evalStmnt (While (V "x" .<=. WL,
+ Seq (Ass ("y", V "y" .+. V "x"),
+ Ass ("x", V "x" .+. N 1))))
+ hello (Map.ofList [("x", 3); ("y", 100)])
+
+// 3.8
+type squareFun = word -> int -> int -> int
+
+let stmntToSquareFun (stm : stmnt) =
+    fun (w : word) (pos : int) (acc : int) -> 
+        let state = evalStmnt stm w (Map.ofList [("_pos_", pos); ("_acc_", acc)])
+        state.Item("_result_")
+
+let singleLetterScore = stmntToSquareFun (Ass ("_result_", arithSingleLetterScore))
+let doubleLetterScore = stmntToSquareFun (Ass ("_result_", arithDoubleLetterScore))
+let tripleLetterScore = stmntToSquareFun (Ass ("_result_", arithTripleLetterScore))
+let doubleWordScore = stmntToSquareFun (Ass ("_result_", arithDoubleWordScore))
+let tripleWordScore = stmntToSquareFun (Ass ("_result_", arithTripleWordScore))
+
+let containsNumbers =
+    stmntToSquareFun
+        (Seq (Ass ("_result_", V "_acc_"),
+        While (V "i" .<. WL,
+        ITE (IsDigit (CV (V "i")),
+        Seq (
+        Ass ("_result_", V "_result_" .*. N -1),
+        Ass ("i", WL)),
+        Ass ("i", V "i" .+. N 1)))))
+
+containsNumbers hello 5 50
+containsNumbers (('0', 100)::hello) 5 50
+containsNumbers (hello @ [('0', 100)]) 5 50
+
+// Red Exercises
+
+// 3.9
