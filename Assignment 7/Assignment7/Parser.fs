b6cf50961354be55568a74a82bfaaa5a4b79078d
Martin <mwha@itu.dk>
2023-06-01 17:43:12 +0200

    More changes

--- Assignment 7/Assignment7/Parser.fs
+++ Assignment 7/Assignment7/Parser.fs
@@ -37,12 +37,12 @@
 
     // 7.2
 
-    let whitespaceChar = satisfy (fun c -> System.Char.IsWhiteSpace c)
-    let pletter = satisfy (fun c -> System.Char.IsLetter c)
-    let palphanumeric  = satisfy (fun c -> System.Char.IsLetterOrDigit c)
+    let whitespaceChar = satisfy (fun c -> System.Char.IsWhiteSpace c) <?> "whitespace"
+    let pletter        = satisfy (fun c -> System.Char.IsLetter c) <?> "letter"
+    let palphanumeric  = satisfy (fun c -> System.Char.IsLetterOrDigit c) <?> "alphanumeric"
 
-    let spaces         = many whitespaceChar
-    let spaces1        = many1 whitespaceChar
+    let spaces         = many whitespaceChar <?> "spaces"
+    let spaces1        = many1 whitespaceChar <?> "space1"
 
     // 7.3
 
@@ -56,29 +56,56 @@
 
     let curlybrackets p = pchar '{' >*>. p .>*> pchar '}'
 
-    let pid = pstring "not implemented"
+    // 7.5
 
+    let pid = 
+        (pchar '_' <|> pletter) .>>. many (pchar '_' <|> palphanumeric) |>> fun (x,y) -> (x::y) |> System.String.Concat
     
-    let unop _ = failwith "not implemented"
-    let binop _ p1 p2 = p1 .>>. p2 // incorrect (not implemented)
+    // 7.6
+
+    let unop (a : Parser<'a>) (p1 : Parser<'b>) = a >*>. p1
+
+    // 7.7
+    
+    let binop a p1 p2 = p1 .>*> a .>*>. p2
+
+    // 7.8
 
     let TermParse, tref = createParserForwardedToRef<aExp>()
     let ProdParse, pref = createParserForwardedToRef<aExp>()
     let AtomParse, aref = createParserForwardedToRef<aExp>()
+    let CTermParse, ctref = createParserForwardedToRef<cExp>()
+    let CProdParse, cpref = createParserForwardedToRef<cExp>()
+    let CAtomParse, caref = createParserForwardedToRef<cExp>()
 
     let AddParse = binop (pchar '+') ProdParse TermParse |>> Add <?> "Add"
-    do tref := choice [AddParse; ProdParse]
+    let SubParse = binop (pchar '-') ProdParse TermParse |>> Sub <?> "Sub"
+    do tref := choice [AddParse; SubParse; ProdParse]
 
     let MulParse = binop (pchar '*') AtomParse ProdParse |>> Mul <?> "Mul"
-    do pref := choice [MulParse; AtomParse]
+    let DivParse = binop (pchar '/') AtomParse ProdParse |>> Div <?> "Div"
+    let ModParse = binop (pchar '%') AtomParse ProdParse |>> Mod <?> "Mod"
+    do pref := choice [MulParse; DivParse; ModParse; AtomParse]
 
+    let NegationParse = unop (pchar '-') AtomParse |>> (fun x -> N -1, x) |>> Mul <?> "Negation"
+    let PVParse = unop pPointValue AtomParse |>> PV <?> "PV"
+    let VParse = pid |>> V <?> "String"
     let NParse   = pint32 |>> N <?> "Int"
     let ParParse = parenthesise TermParse
-    do aref := choice [NParse; ParParse]
+    let CharToIntParse = unop pCharToInt CTermParse |>> CharToInt <?> "charToInt"
+    do aref := choice [CharToIntParse; NegationParse; NParse; PVParse; VParse; ParParse]
 
-    let AexpParse = TermParse 
+    let AexpParse = TermParse      
 
-    let CexpParse = pstring "not implemented"
+    let CParse = pchar ''' >>. anyChar .>> pchar ''' |>> C <?> "C"
+    let CParParse = parenthesise CTermParse
+    let CVParse = unop pCharValue AtomParse |>> CV <?> "CV"
+    let ToUpperParse = unop pToUpper CTermParse |>> ToUpper <?> "toUpper"
+    let ToLowerParse = unop pToLower CTermParse |>> ToLower <?> "toLower"
+    let IntToCharParse = unop pIntToChar AtomParse |>> IntToChar <?> "intToChar"
+    do ctref := choice [ToLowerParse; ToUpperParse; IntToCharParse; CParse; CVParse; CParParse]
+
+    let CexpParse = CTermParse
 
     let BexpParse = pstring "not implemented"
 
